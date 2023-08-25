1faef3219719545064e4814e476c10f6fba20530
dave445 <dawitdemisew9@gmail.com>
2023-08-25 17:33:50 +0300

    first

--- jobs/app/index.js
+++ jobs/app/index.js
@@ -1,10 +1,26 @@
-import { View, Text} from 'react-native';
+import { useState } from 'react';
+import { View, ScrollView, SafeAreaView } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+
+import {COLORS, icons, images, SIZES} from '../constants'; 
 
 const Home = () =>{
+    const router = useRouter();
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
+        <SafeAreaView style={{ flex:1, backgroundColor:COLORS.lightWhite }}>
+            <Stack.Screen 
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightWhite},
+                    headerShadowVisible: false,
+                    headerLeft: () =>(
+                        <ScreenHeaderBtn iconUrl={icons.menu} dimesnsion="60%"/>
+                    ),
+                    headerRight: () =>(
+                        <ScreenHeaderBtn iconUrl={icons.menu} dimesnsion="100%"/>
+                    ),
+                }}
+            />
+        </SafeAreaView>
     )
 }
 
